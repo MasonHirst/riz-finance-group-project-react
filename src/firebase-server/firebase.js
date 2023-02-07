247f5847d802109fc7cff0e3f5b5fbfc2b73607e
Mason Hirst <mason.j.hirst@gmail.com>
2023-02-07 11:05:27 -0700

    added two folders and the firebase setup file

---
+++ src/firebase-server/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+const { initializeApp } = require('firebase/app')
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyDxM3zYXej6kdPcoy7hCG6YxM-YaaXFJPY',
+  authDomain: 'riz-finance-1cb33.firebaseapp.com',
+  projectId: 'riz-finance-1cb33',
+  storageBucket: 'riz-finance-1cb33.appspot.com',
+  messagingSenderId: '658018559135',
+  appId: '1:658018559135:web:b8885762e019caa4e7aeaa',
+  measurementId: 'G-Q2VBVHLD1S',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+//
