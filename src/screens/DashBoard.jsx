dd874dc95824fde2f7906da9b1295a4010f2d466
Marcus Johnson <marcusjohnson88.mj@gmail.com>
2023-02-07 13:30:15 -0700

    got rid of css

--- src/screens/DashBoard.jsx
+++ src/screens/DashBoard.jsx
@@ -1,34 +1,33 @@
 import React from "react";
 
-import styles from "../UI/DashBoard.module.css";
 
 // TODO This is just the skeleton for what I need to put in. I still need MUI to actually create the dashboard page.
 
 const DashBoard = () => {
   return (
     <div>
-      <header className={styles.header}>
-        <nav className={styles.nav}>
+      <header>
+        <nav>
           <h2>About</h2>
           <h2>Help</h2>
           <h2>Contact Us</h2>
         </nav>
       </header>
-      <main className={styles.snapshot_container}>
-        <div className={styles.snapshot_left}>
-          <div className={styles.snapshot_box_left}>
-            <h3 className={styles.snap_headers}>Trends</h3>
+      <main>
+        <div>
+          <div>
+            <h3>Trends</h3>
           </div>
-          <div className={styles.snapshot_box_left}>
-            <h3 className={styles.snap_headers}>Goals</h3>
+          <div>
+            <h3>Goals</h3>
           </div>
         </div>
-        <div className={styles.snapshot_right}>
-          <div className={styles.snapshot_box}>
-            <h3 className={styles.snap_headers_right}>Budget</h3>
+        <div>
+          <div>
+            <h3>Budget</h3>
           </div>
-          <div className={styles.snapshot_box}>
-            <h3 className={styles.snap_headers_right}>Transactions</h3>
+          <div>
+            <h3>Transactions</h3>
           </div>
         </div>
       </main>
